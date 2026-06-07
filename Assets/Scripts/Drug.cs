7710e32581e2f057a58ff656df0feadae79ba7c8
Aldo <aldo.x225@gmail.com>
2026-06-07 17:10:53 -0400

    Updated Drug class script

--- Assets/Scripts/Drug.cs
+++ Assets/Scripts/Drug.cs
@@ -5,12 +5,13 @@ public class Drug
     public string Name;
     public int Strength;
     public double NDC;
-    public bool Active = true;
+    public bool IsActive;
 
     public Drug(string name, int strength, double ndc)
     {
         Name = name;
         Strength = strength;
         NDC = ndc;
+        IsActive = true;
     }
 }
